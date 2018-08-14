512b9af463cb215b0ad16ae34a48fe613fcc7a2a
Piotr Żak <piotrzak77@gmail.com>
2018-08-14 22:44:35 +0200

    Payment Scheme

--- client_oktawian/projekt/src/_helpers/auth-header.js
+++ client_oktawian/projekt/src/_helpers/auth-header.js
@@ -3,7 +3,9 @@ export function authHeader() {
     // return authorization header with jwt token
     const user = JSON.parse(localStorage.getItem('user'));
 
-    console.log('Auth Token', user.auth_token);
+    const userparsed = JSON.parse(user)
+
+    console.log('Auth Token', userparsed.auth_token);
     console.log(user)
         return { 'Authorization': 'Bearer ' + user.token };
 }
--- client_oktawian/projekt/src/global-styles.js
+++ client_oktawian/projekt/src/global-styles.js
@@ -516,4 +516,25 @@ margin:3% 0 3% 0;
   margin:1% 0 1% 0;
   }
 
+
+
+  .DepositSteps{
+    margin:5% 0 5% 0;
+  }
+
+
+
+
+  // PAYMENT
+
+
+  .PaymentHeader{
+    background-image: linear-gradient(to right, #6a11cb 0%, #2575fc 100%);
+    width:100%;
+    height:80px;
+  }
+
+
+
+
 `;
