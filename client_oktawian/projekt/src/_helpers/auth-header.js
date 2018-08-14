1de5d505a9aff2df980686642b347ca9ef22a819
Piotr Żak <piotrzak77@gmail.com>
2018-08-14 22:57:51 +0200

    Now Submit Fund display 200, 204

--- client_oktawian/projekt/src/_helpers/auth-header.js
+++ client_oktawian/projekt/src/_helpers/auth-header.js
@@ -2,10 +2,7 @@
 export function authHeader() {
     // return authorization header with jwt token
     const user = JSON.parse(localStorage.getItem('user'));
-
     const userparsed = JSON.parse(user)
 
-    console.log('Auth Token', userparsed.auth_token);
-    console.log(user)
-        return { 'Authorization': 'Bearer ' + user.token };
+        return { 'Authorization': 'Bearer ' + userparsed.auth_token };
 }
